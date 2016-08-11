919e9819c82f834a16fb61d4cf8e7cff4f0c4f7e
Kevin Wong <kevinwong_895@hotmail.com>
2016-08-11 09:30:45 -0700

    Editing insertionSort

--- src/sort.java
+++ src/sort.java
@@ -36,15 +36,18 @@ public class sort {
 	}
 	
 	public static void insertionSort(int[] arr){
-		int temp;
-		for(int i = 1; i < arr.length; i++){
-			for(int j = 0; j > 0; j--){
-				temp = arr[j];
-				arr[j] = arr[j-1];
-				arr[j-1] = temp;
-			}
-		}
-		printArray(arr);
+        int n = arr.length;
+        for (int j = 1; j < n; j++) {
+            int key = arr[j];
+            int i = j-1;
+            while ( (i > -1) && ( arr [i] > key ) ) {
+                arr [i+1] = arr [i];
+                i--;
+            }
+            arr[i+1] = key;
+        }
+        printArray(arr);
+
 	}
 	
 	public static void printArray(int[] array){
@@ -55,7 +58,7 @@ public class sort {
 	}
 	
 	public static void main(String args[]){
-		int[] array = new int[]{1,7,9,34,54};
+		int[] array = new int[]{1,43,1,44,54};
 		insertionSort(array);
 	}
 }
