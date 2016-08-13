10be1325b0ad91ec35aa851fb32973efb3eb94d8
Kevin Wong <kevinwong_895@hotmail.com>
2016-08-12 23:45:00 -0700

    Learing radixSort

--- src/sort.java
+++ src/sort.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class sort {
 
@@ -57,8 +60,43 @@ public class sort {
 		}
 	}
 	
+	 public static void radixSort(int[] input) {
+		  final int RADIX = 10;
+		  // declare and initialize bucket[]
+		  List<Integer>[] bucket = new ArrayList[RADIX];
+		  for (int i = 0; i < bucket.length; i++) {
+		    bucket[i] = new ArrayList<Integer>();
+		  }
+		 
+		  // sort
+		  boolean maxLength = false;
+		  int tmp = -1, placement = 1;
+		  while (!maxLength) {
+		    maxLength = true;
+		    // split input between lists
+		    for (Integer i : input) {
+		      tmp = i / placement;
+		      bucket[tmp % RADIX].add(i);
+		      if (maxLength && tmp > 0) {
+		        maxLength = false;
+		      }
+		    }
+		    // empty lists into input array
+		    int a = 0;
+		    for (int b = 0; b < RADIX; b++) {
+		      for (Integer i : bucket[b]) {
+		        input[a++] = i;
+		      }
+		      bucket[b].clear();
+		    }
+		    // move to next digit
+		    placement *= RADIX;
+		  }
+		  printArray(input);
+		}
+	
 	public static void main(String args[]){
 		int[] array = new int[]{1,43,1,44,54};
-		insertionSort(array);
+		radixSort(array);
 	}
 }
